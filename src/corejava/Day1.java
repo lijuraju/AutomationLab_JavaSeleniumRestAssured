d5ab1c4e9045fcbccf59ca504aeb1bda2e83f7e9
lijuraju <liju.raju@philips.com>
2024-09-06 11:35:12 +0530

    Sample pgms commiting

---
+++ src/corejava/Day1.java
@@ -0,0 +1,14 @@
+package corejava;
+
+public class Day1 {
+
+    public static void main(String[] args2 ) {
+        System.out.println("hai");
+        main2(4);
+
+    }
+    public static void main2(int args2 ) {
+        System.out.println(args2);
+
+    }
+}
