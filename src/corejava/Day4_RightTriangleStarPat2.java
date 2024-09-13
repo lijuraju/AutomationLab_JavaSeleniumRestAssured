9aec778f1843aea62a0f53bf0a90870826edd78b
lijuraju <liju.raju@philips.com>
2024-09-13 08:25:23 +0530

    Sample pgms commiting

---
+++ src/corejava/Day4_RightTriangleStarPat2.java
@@ -0,0 +1,18 @@
+package corejava;
+
+public class Day4_RightTriangleStarPat2 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j =1; j<=i; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
