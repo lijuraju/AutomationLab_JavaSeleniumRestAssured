1a0ce184634569f159f05223e3140b0072a9b7ca
lijuraju <liju.raju@philips.com>
2024-09-20 09:38:40 +0530

    20Sep2024

---
+++ src/corejava/sep202024/Lab04_TernaryOperatormax.java
@@ -0,0 +1,13 @@
+package corejava.sep202024;
+
+public class Lab04_TernaryOperatormax {
+    public static void main(String[] args) {
+        int num1 = 40;
+        int num2 = 20;
+        int num3 = 30;
+
+
+        int max = (num1>num2) ? (num1>num3?num1:num3) : (num2>num3?num2:num3);
+        System.out.println("Max is: "+max);
+    }
+}
