568131ec2efd4aab3192ee644524c91475a370cd
lijuraju <liju.raju@philips.com>
2024-09-20 10:17:13 +0530

    20Sep2024

--- src/corejava/sep202024/Lab05_GradeCalculator_TernaryOperator.java
+++ src/corejava/sep202024/Lab05_GradeCalculator_TernaryOperator.java
@@ -2,10 +2,10 @@ package corejava.sep202024;
 
 public class Lab05_GradeCalculator_TernaryOperator {
     public static void main(String[] args) {
-        int score =20;
+        int score =70;
         String grade;
 
-       grade = (score>90) ? "A" : (score>80)? "B" : (score>70)? "C": (score>60)? "D" : "F";
+       grade = ((score>=90) ? "A" : (score>=80)? "B" : (score >=70)? "C": (score>=60)? "D" : "F");
         System.out.println(grade);
     }
 }
