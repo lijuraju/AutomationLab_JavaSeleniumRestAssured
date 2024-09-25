9e208f1ce436631dff4041046cdf9b7474c07423
lijuraju <liju.raju@philips.com>
2024-09-25 08:10:03 +0530

    25Sep2024

---
+++ src/corejava/sep242024/Lab03_ReverseStringSimple.java
@@ -0,0 +1,23 @@
+package corejava.sep242024;
+
+import java.util.Scanner;
+
+public class Lab03_ReverseStringSimple {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String input = sc.nextLine().toLowerCase();
+        sc.close();
+
+        String reversed = "";
+
+        for (int i =input.length()-1;i>=0;i--)
+        {
+            reversed = reversed + input.charAt(i);
+
+        }
+        System.out.println("Reversed String: " + reversed);
+
+    }
+}
