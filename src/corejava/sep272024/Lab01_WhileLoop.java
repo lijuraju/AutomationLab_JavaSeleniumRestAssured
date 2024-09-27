56da440e7adc87070b5cd31c5f162ea38671894b
lijuraju <liju.raju@philips.com>
2024-09-27 13:00:08 +0530

    27Sep2024

---
+++ src/corejava/sep272024/Lab01_WhileLoop.java
@@ -0,0 +1,13 @@
+package corejava.sep272024;
+
+public class Lab01_WhileLoop {
+    public static void main(String[] args) {
+        int i=1;
+
+        while(i<1)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
---
+++ src/corejava/sep272024/Task03_Sumof100Numbers.java
@@ -0,0 +1,16 @@
+package corejava.sep272024;
+
+public class Task03_Sumof100Numbers {
+    public static void main(String[] args) {
+
+        int i=1;
+        int sum=0;
+
+        while(i<=100){
+
+            sum = sum + i;
+            i++;
+        }
+        System.out.println("Sum of 1 to 5 is: " + sum);
+    }
+}
