f503ee77d1eafef3552b7abb23694d16ad5fa433
lijuraju <liju.raju@philips.com>
2024-10-03 07:35:05 +0530

    03Oct2024

---
+++ src/corejava/oct022024/ClassAndObjects.java
@@ -0,0 +1,18 @@
+package corejava.oct022024;
+
+public class ClassAndObjects {
+
+    //1.Class is a blueprint or template for creating objects
+    //2.Class contains data members and member functions
+    //3.Class is a logical entity
+    //4.Class does not occupy memory space
+    //5.Class is a user defined data type.
+
+    //1.Object is an instance of a class
+    //2.Object is a physical entity
+    //3.Object occupies memory space
+    //4.Object is a real time entity
+    //5.Object is a reference of a class
+    //6.Object is a run time entity
+
+}
---
+++ src/corejava/oct022024/OOPs.java
@@ -0,0 +1,13 @@
+package corejava.oct022024;
+
+public class OOPs {
+
+    //OOPs is a way of designing a program using classes and objects.
+    //Main features are
+    //1)Classes and Objects
+    //2)Encapsulation
+    //3)Inheritance
+    //4)Polymorphism
+    //5)Abstraction
+
+}
