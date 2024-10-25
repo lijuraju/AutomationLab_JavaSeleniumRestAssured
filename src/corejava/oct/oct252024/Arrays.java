ed6b96d023b40a6c90e442989bd7cd631fbe514a
lijuraju <liju.raju@philips.com>
2024-10-25 08:08:19 +0530

    Arrays

---
+++ src/corejava/oct/oct252024/Arrays.java
@@ -0,0 +1,20 @@
+package corejava.oct.oct252024;
+
+public class Arrays {
+
+    public static void main(String[] args) {
+
+        int[] array = {1,2,3,4,5};
+
+        //System.out.println(array.length);
+
+        int[] array1 = new int[5];
+
+        array1[0] =1;
+
+        java.util.Arrays.sort(array);
+
+        System.out.println(array);
+
+    }
+}
